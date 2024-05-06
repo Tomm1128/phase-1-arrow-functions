53dac079a03b1709e3633113761e55c6570e0b66
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-06 13:56:34 -0400

    Complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+// First function should be a regular function to divide two numbers.
+
+function divide (num1, num2){
+        return num1 / num2;
+}
+
+// Here is a arrow function of the function above
+// const divide = (num1, num2) => num1 / num2;
+
+// Arrow function to square a number
+const square = num1 => num1 * num1;
+
+// Arrow function to add two numbers
+const add = (num1, num2) => num1 + num2;
